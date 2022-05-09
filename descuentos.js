599eeed5a414a7f322892556f3a6773794d288d8
José Carlomagno <jos3.74502@gmai.com>
2022-05-08 22:07:31 -0300

    proyecto de descuentos terminado

--- descuentos.js
+++ descuentos.js
@@ -1,6 +1,11 @@
 /* const precioOriginal = 120;
 const descuento = 18; */
 
+const coupons = [
+    "Cupon10",
+    "Cupon20",
+    "Cupon50"
+];
 
 function calcularPrecioConDescuento(precio, descuento) {
     const porcentajePrecioConDescuento = 100 - descuento;
@@ -9,7 +14,21 @@ function calcularPrecioConDescuento(precio, descuento) {
     return precioConDescuento;
 }
 
+function onClickButtonPriceDiscount(){
+   const inputPrice = document.getElementById("InputPrice");
+   const priceValue = inputPrice.value; 
 
+   const inputCoupon = document.getElementById("InputCoupon");
+   const couponValue = inputCoupon.value; 
+
+   const precioConDescuento = calcularPrecioConDescuento(priceValue, couponValue);
+
+   const resultP = document.getElementById("ResultP");
+   console.log(resultP);
+   resultP.innerText = "El precio con descuento es : $" + precioConDescuento;
+}
+
+                                                                                                             
 
 //console.log({
     //precioOriginal,
